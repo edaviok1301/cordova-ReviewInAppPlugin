b201c0e28a48cd289ae3064cd04df1b78c9a4097
edaviok1301 <eparedesba1301@gmail.com>
2022-01-07 11:56:24 -0500

    go

--- src/android/ReviewInAppPluginClass.java
+++ src/android/ReviewInAppPluginClass.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import com.google.android.play.core.review.ReviewInfo;
 import com.google.android.play.core.review.ReviewManager;
 import com.google.android.play.core.review.ReviewManagerFactory;
+import com.google.android.play.core.review.testing.FakeReviewManager;
 import com.google.android.play.core.tasks.Task;
 
 import org.apache.cordova.CordovaPlugin;
@@ -23,8 +24,17 @@ public class ReviewInAppPluginClass extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("requestReview")) {
-            String message = args.getString(0);
-            this.requestReview(message, callbackContext);
+
+            Boolean isFake = args.getBoolean(0);
+
+            cordova.getThreadPool().execute(new Runnable() {
+                @Override
+                public void run() {
+                    requestReview(isFake, callbackContext);
+                }
+            });
+
+
             return true;
         }
         return false;
@@ -32,17 +42,40 @@ public class ReviewInAppPluginClass extends CordovaPlugin {
 
     private void requestReview(String message, CallbackContext callbackContext) {
 
-        ReviewManager manager = ReviewManagerFactory.create(cordova.getContext());
+        ReviewManager manager;
+        if(isFake){
+            Log.d(LOG,"isFake");
+            manager = new FakeReviewManager(cordova.getContext());
+        }else {
+            Log.d(LOG,"notIsFake");
+            manager = ReviewManagerFactory.create(cordova.getContext());
+        }
 
         Task<ReviewInfo> request = manager.requestReviewFlow();
         request.addOnCompleteListener(task -> {
            if(task.isComplete()){
                ReviewInfo reviewInfo = task.getResult();
+               Task<Void> flow=manager.launchReviewFlow(this.cordova.getActivity(),reviewInfo);
+               flow.addOnCompleteListener(taskflowsuccess->{
+                   if(taskflowsuccess.isComplete()){
+                       Log.d(LOG,"Completado");
+                       if(taskflowsuccess.isSuccessful()){
+                           Log.d(LOG,"Proceso exitoso");
+                           callbackContext.success();
+                       }else{
+                           callbackContext.success();
+                       }
+                   }
+               }).addOnFailureListener(taskflowfail->{
+                   callbackContext.error(taskflowfail.getMessage());
+               });
                Log.d(LOG,reviewInfo.toString());
            }else{
                String reviewErrorCode =  task.getException().toString();
                Log.d(LOG,reviewErrorCode);
            }
+        }).addOnFailureListener(taskfail->{
+            callbackContext.error(taskfail.getMessage());
         });
     }
 }
--- www/cordova-ReviewInAppPlugin.js
+++ www/cordova-ReviewInAppPlugin.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.requestReview = function (arg0, success, error) {
-    exec(success, error, 'cordova-ReviewInAppPlugin', 'coolMethod', [arg0]);
+    exec(success, error, 'cordova-ReviewInAppPlugin', 'requestReview', [arg0]);
 };
